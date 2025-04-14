b62290c7160af3bc02c8d0ab323b8b589f704d73
Alireza-Alipoor <thealipoor@gmail.com>
2025-04-14 18:50:14 +0330

    feat: add fitness evaluation function

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from random import randint
+from math import comb
+from itertools import combinations
 
 
 N = 8
-number_of_genoms = 50
+number_of_chroms = 50
+max_fitness = comb(N, 2)
 
 population = [[randint(1, N) for _ in range(N)]
-              for _ in range(number_of_genoms)]
+              for _ in range(number_of_chroms)]
+
+
+def fitness(chrom: list[int]) -> int:
+    score = max_fitness
+    for i, j in combinations(range(N), 2):
+        if chrom[i] == chrom[j] or abs(i-j) == abs(chrom[i]-chrom[j]):
+            score -= 1
+    return score
